f9f231bbbbe89aaa5d7e24f34d15431f141ae888
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-05-04 09:44:49 +0200

    created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+        <Route path="/contact" element={<div>Contact</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
