18511b401843544897eac5def1dce5181e2489b1
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-05-13 11:33:20 +0200

    About page

---
+++ src/pages/About.tsx
@@ -0,0 +1,15 @@
+import NavBar from "@/components/navBar"
+import Footer from "@/components/footer"
+
+export default function About(){
+
+return(
+    <>
+        <NavBar />
+
+        <h1>About Page</h1>
+
+        <Footer/>
+    </>
+)
+}
